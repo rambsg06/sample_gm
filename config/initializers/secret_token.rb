bf00030a1315640e571fa9710595bc65181be0c5
Rameswar Rath <ram@gmicro.us>
2013-02-23 15:48:54 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleGm::Application.config.secret_token = '6aa22f8c858ca001208f77918cc5d07d80cdbfc4579e3a25a12593cc156e582cef3d8b7417ef28164d430c8046c26030610027f17a521e938423232d8f79ef0f'
